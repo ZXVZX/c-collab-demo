17a1528bec484d7f8c707b9161cdced72dcab390
LOFI <aa0938955781@gmail.com>
2025-12-05 10:54:48 +0800

    feat: add subtraction

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+int sub(int a, int b) { return a - b; } // Role C
 int main() {
     printf("=== C Language Calculator ===\n");
-    // Operations placeholders
+    printf("50 - 30 = %d\n", sub(50, 30)); // Role C
     printf("System Check OK\n");
     return 0;
 }
